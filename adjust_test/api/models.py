3fa6a69a4b323964a56dfb5aec793310a44b9ffb
Botifier <mouath.sai.ensi@gmail.com>
2019-07-29 22:32:51 +0200

    models: add SampleDataSet

--- adjust_test/api/models.py
+++ adjust_test/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class SampleDataSet(models.Model):
+    date = models.DateField()
+    channel = models.CharField(max_length=32)
+    country = models.CharField(max_length=2)
+    os = models.CharField(max_length=32)
+    impressions = models.IntegerField()
+    clicks = models.IntegerField()
+    installs = models.IntegerField()
+    spend = models.FloatField()
+    revenue = models.FloatField()
+
+    @property
+    def cpi(self):
+        return self.spend / self.installs
