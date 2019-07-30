dadb9bbf73134dfd3f444c27f7b28ea6bc690fef
Botifier <mouath.sai.ensi@gmail.com>
2019-07-30 02:45:15 +0200

    update settings, change base_dir

--- adjust_test/adjust_test/settings/production.py
+++ adjust_test/adjust_test/settings/production.py
@@ -1,4 +1,4 @@
-from .settings import *
+from .develop import *
 
 DEBUG = False
 
--- adjust_test/manage.py
+++ adjust_test/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adjust_test.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adjust_test.settings.develop")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
